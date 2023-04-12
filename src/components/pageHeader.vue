6785cc3d207f745aa63a1bdb7ce2fa4cb1625789
Kevin La <chenchiaho@gmail.com>
2023-04-12 18:07:07 +0800

    update

--- src/components/pageHeader.vue
+++ src/components/pageHeader.vue
@@ -1,12 +1,21 @@
 <template>
-  <header>
-    <h1>Task Tracker</h1>
+  <header> 
+    <h1>{{ title }}</h1>
+    <defaultButton text="Add Task" color="green"/>
   </header>
 </template>
 
 <script>
+import defaultButton from './defaultButton.vue'
+
 export default {
     name: 'page-header',
+    props: {
+      title: String,
+    },
+    components: {
+      defaultButton
+    }
   }
 </script>
 
